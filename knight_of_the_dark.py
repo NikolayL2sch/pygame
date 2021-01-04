99129c9d37fcbf87f6560742f9433b4a2c01e9f8
Nikolay Milenko <31999179+NikolayL2sch@users.noreply.github.com>
2021-01-04 22:39:48 +0300

    Update and rename moveable_rect.py to knight_of_the_dark.py

--- knight_of_the_dark.py
+++ knight_of_the_dark.py
@@ -7,7 +7,7 @@ retval = os.getcwd()
 print ("Directory changed successfully %s" % retval)
 pygame.init()
 
-win = pygame.display.set_mode((500,500))
+win = pygame.display.set_mode((1920,1080))
 
 pygame.display.set_caption('my first game')
 
@@ -25,18 +25,18 @@ pygame.image.load('pics/run4.png'), pygame.image.load('pics/run5.png')]
 jump = [pygame.image.load('pics/jump0.png'), pygame.image.load('pics/jump1.png'),
 pygame.image.load('pics/jump2.png'), pygame.image.load('pics/jump3.png')]
 
-bg = pygame.image.load('pics/bg.jpg')
+bg = pygame.image.load('bg.png')
 
 idle = [pygame.image.load('pics/idle0.png'), pygame.image.load('pics/idle1.png'),
 pygame.image.load('pics/idle2.png'), pygame.image.load('pics/idle3.png')]
 
 clock = pygame.time.Clock()
 
-x = 350
-y = 350
+x = 100
+y = 600
 width = 50
 height = 37
-velocity = 5
+velocity = 20
 
 isJump = False
 jumpcnt = 10
@@ -44,31 +44,38 @@ left = False
 right = False
 walkCount = 0
 idleCount = 0
-
+jumpCount = 0
 
 def redraw_Win():
-    global walkCount, idleCount
+    global walkCount, idleCount,jumpCount
     win.blit(bg,(0,0))    
-    if walkCount +1 >= 30:
+    if walkCount +1 >= 60:
         walkCount = 0
-        idleCount = 0
     if left:
-        win.blit(runl[walkCount//5],(x,y))
+        win.blit(runl[walkCount//10],(x,y))
         walkCount+=1
     elif right:
-        win.blit(runr[walkCount//5],(x,y))
+        win.blit(runr[walkCount//10],(x,y))
         walkCount+=1
     elif Idle:
-        if idleCount>3:
-            idleCount%=3
-        win.blit(idle[idleCount//3],(x,y))
-        idleCount+=1
+        if idleCount + 1 > 60:
+            idleCount = 0
+        else:
+            win.blit(idle[idleCount//15],(x,y))
+            idleCount+=1
+    else:
+        if jumpCount + 1 > 60:
+            jumpCount = 0
+        else:
+            win.blit(jump[jumpCount//15],(x,y))
+            jumpCount+=1
+
     pygame.display.update()
 
 #mainloop
 run = True
 while run:
-    clock.tick(30)
+    clock.tick(60)
 
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
@@ -82,7 +89,7 @@ while run:
         right = False
         idleCount = 0
         Idle = False
-    elif keys[pygame.K_RIGHT] and x<=500-width-velocity:
+    elif keys[pygame.K_RIGHT] and x<=700-width-velocity:
         x+=velocity
         right = True
         left = False
@@ -106,7 +113,7 @@ while run:
             neg = 1
             if jumpcnt < 0:
                 neg = -1
-            y-= (jumpcnt ** 2)*0.5 * neg
+            y-= 2*(jumpcnt ** 2)*0.5 * neg
             jumpcnt-= 1 
         else:
             isJump = False
