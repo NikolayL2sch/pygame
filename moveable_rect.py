fa06777c5d6772c2aaf8bdff245ed4fa7d820502
Nikolay Milenko <31999179+NikolayL2sch@users.noreply.github.com>
2021-01-04 11:30:03 +0300

    Update moveable_rect.py

--- moveable_rect.py
+++ moveable_rect.py
@@ -5,11 +5,14 @@ win = pygame.display.set_mode((500,500))
 
 pygame.display.set_caption('my first game')
 
-x = 50
-y = 50
+x = 350
+y = 350
 width = 40
 height = 60
 velocity = 5
+
+isJump = False
+jumpcnt = 10
 
 run = True
 while run:
@@ -21,14 +24,23 @@ while run:
 
     keys = pygame.key.get_pressed()
 
-    if keys[pygame.K_LEFT]:
+    if keys[pygame.K_LEFT] and x>=velocity:
         x-=velocity
-    if keys[pygame.K_RIGHT]:
+    if keys[pygame.K_RIGHT] and x<=500-width-velocity:
         x+=velocity
-    if keys[pygame.K_DOWN]:
-        y+=velocity
-    if keys[pygame.K_UP]:
-        y-=velocity
+    if not(isJump):
+        if keys[pygame.K_SPACE]:
+            isJump = True
+    else:
+        if jumpcnt >= -10:
+            neg = 1
+            if jumpcnt < 0:
+                neg = -1
+            y-= (jumpcnt ** 2)*0.5 * neg
+            jumpcnt-= 1 
+        else:
+            isJump = False
+            jumpcnt = 10
     win.fill((0,0,0))    
     pygame.draw.rect(win,(229,111,190),(x,y,width,height))
     pygame.display.update()
